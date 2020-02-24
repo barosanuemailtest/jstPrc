5d59a12694baf1f55fa8a678c8aa3d8f83882497
Dan Alexandru <Alexandru.Dan@elektrobit.com>
2020-02-24 11:58:55 +0200

    request impl

--- src/app/Authorizer.ts
+++ src/app/Authorizer.ts
@@ -1,7 +1,20 @@
-import * as https from 'https';
-import { RequestOptions } from 'https';
-import { URL } from 'url';
+import { get } from 'request-promise';
+
+export enum AccessRights {
+    READ_BASIC_INFO = 'READ_BASIC_INFO',
+    READ_EXTENDED_INFO = 'READ_EXTENDED_INFO',
+    CHANGE_BASIC_DATA = 'CHANGE_BASIC_DATA',
+    CHANGE_EXTENDED_DATA = 'CHANGE_EXTENDED_DATA'
+}
 
 export class Authorizer {
-    private authorizerUrl = process.env.AUTHORIZER_URL;
+    private authorizerUrl = process.env.AUTHORIZER_URL!;
+
+    public async getTokenRights(token: string): Promise<AccessRights[]> {
+        const accessRights: AccessRights[] = [];
+
+        const result = await get(this.authorizerUrl, { body: { token: token } });
+
+        return accessRights;
+    }
 }
