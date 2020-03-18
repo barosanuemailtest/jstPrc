85461be1b1facd93a2ee3b6de321bb39f75f8d9e
barosanuemailtest <barosanu.email.test@gmail.com>
2020-03-18 19:32:55 +0200

    Server utils test

--- test/Utils.test.ts
+++ test/Utils.test.ts
@@ -1,8 +1,19 @@
 import { Utils } from '../src/app/Server/Utils';
+import { UrlWithParsedQuery } from 'url';
 
 describe.only('ServerUtils test', () => {
 
-    beforeEach(() => {
+    let parsedUrl: UrlWithParsedQuery;
+
+    test('parse simple url', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login');
+        expect(parsedUrl.host).toBe('localhost:8080');
+        expect(parsedUrl.hostname).toBe('localhost');
+        expect(parsedUrl.href).toBe('http://localhost:8080/login');
+        expect(parsedUrl.path).toBe('/login');
+        expect(parsedUrl.port).toBe('8080');
+        expect(parsedUrl.protocol).toBe('http:');
+        expect(parsedUrl.query).toBeNull();
 
     })
 
