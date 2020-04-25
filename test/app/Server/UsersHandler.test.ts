3fb0833f2dc499729fec37cc82bee376ac25c65d
Dan Alexandru <Alexandru.Dan@elektrobit.com>
2020-04-25 16:57:20 +0300

    Users hanler test

--- test/app/Server/UsersHandler.test.ts
+++ test/app/Server/UsersHandler.test.ts
@@ -1,6 +1,7 @@
 import { UsersHandler } from '../../../src/app/Server/UsersHandler';
 import { TokenRights, TokenState } from '../../../src/app/Authorization/Model';
-import { HTTP_CODES } from '../../../src/app/Server/Model';
+import { HTTP_CODES, OperationState } from '../../../src/app/Server/Model';
+import { User } from '../../../src/app/User/Model';
 
 describe('UsersHandler test suite', () => {
     let usersHandler: UsersHandler;
@@ -13,7 +14,8 @@ describe('UsersHandler test suite', () => {
         url: '',
         headers: {
             authorization: 'authorization'
-        }
+        },
+        on: jest.fn()
     }
 
     const responseMock = {
@@ -22,7 +24,8 @@ describe('UsersHandler test suite', () => {
     }
 
     const usersControllerMock = {
-        deleteUser: jest.fn()
+        deleteUser: jest.fn(),
+        updateUser: jest.fn()
     }
 
     beforeEach(() => {
@@ -45,7 +48,29 @@ describe('UsersHandler test suite', () => {
         state: TokenState.INVALID
     }
 
-    test('deleteUser', async () => {
+    const noRightsOperation: OperationState = {
+        authorized: false,
+        tokenState: noRightsToken.state
+    }
+
+    const someUser: User = {
+        age: 23,
+        email: 'some@user.com',
+        id: '1233sdf',
+        name: 'sefu',
+        workingPosition: 2
+    }
+
+    test('deleteUser - no valid token', async () => {
+        requestMock.url = '/users/delete?id=someId';
+        authorizerMock.getTokenRights.mockReturnValueOnce(noRightsToken);
+        await usersHandler.handleRequest();
+        expect(usersControllerMock.deleteUser).toBeCalledTimes(0);
+        expect(responseMock.writeHead).toBeCalledWith(HTTP_CODES.UNAUTHORIZED, { 'Content-Type': 'application/json' });
+        expect(responseMock.write).toBeCalledWith(JSON.stringify(noRightsOperation));
+    })
+
+    test('deleteUser - valid token', async () => {
         requestMock.url = '/users/delete?id=someId';
         authorizerMock.getTokenRights.mockReturnValueOnce(allRightsToken);
         await usersHandler.handleRequest();
@@ -53,5 +78,37 @@ describe('UsersHandler test suite', () => {
         expect(responseMock.writeHead).toBeCalledWith(HTTP_CODES.OK);
         expect(responseMock.write).toBeCalledWith('user someId deleted');
     })
+
+    test('updateUser - no valid token', async () => {
+        requestMock.url = '/users/update';
+        authorizerMock.getTokenRights.mockReturnValueOnce(noRightsToken);
+        await usersHandler.handleRequest();
+        expect(usersControllerMock.updateUser).toBeCalledTimes(0);
+        expect(responseMock.writeHead).toBeCalledWith(HTTP_CODES.UNAUTHORIZED, { 'Content-Type': 'application/json' });
+        expect(responseMock.write).toBeCalledWith(JSON.stringify(noRightsOperation));
+    })
+
+    test('updateUser - valid token', async () => {
+        requestMock.url = '/users/update';
+        authorizerMock.getTokenRights.mockReturnValueOnce(allRightsToken);
+        requestMock.on.mockImplementation(
+            (event: string, cb: any) => {
+                switch (event) {
+                    case 'data':
+                        cb(JSON.stringify(someUser))
+                        break;
+                    case 'end':
+                        cb()
+                        break;
+                }
+            }
+        )
+        await usersHandler.handleRequest();
+        expect(usersControllerMock.updateUser).toBeCalledWith(someUser);
+        expect(responseMock.writeHead).toBeCalledWith(HTTP_CODES.CREATED);
+        expect(responseMock.write).toBeCalledWith(`user ${someUser.name} with id ${someUser.id} updated`);
+    })
+
+
 
 });
